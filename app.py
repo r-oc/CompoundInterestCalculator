2c60f5c40bda1ecd47fe604d754b6058dbbb0a2c
Ryan O'Callaghan <127250776+r-oc@users.noreply.github.com>
2023-08-14 21:04:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,20 @@ import yfinance as yf
 import pandas as pd
 import streamlit as st
 
+# TODO:
+#   - USER INPUT
+#   -
+
 def main():
     # purchase_year = str(input("Enter the year you bought the stocks (defaults to first available date): "))
     # sell_year = str(input("Enter the year you sold the stocks (defaults to today's date): "))
     # ticker = str(input("Enter the stock (ticker): "))
     # shares = input("Enter the total shares you own: ")
+
+    st.sidebar.header("Enter the symbol ticker you want to look at...")
+    start_date = st.sidebar.text_input("Start Date", "1998-01-01")
+    end_date = st.sidebar.text_input("End Date", "2023-08-09")
+    stock_symbol = st.sidebar.text_input("Stock Symbol", "AAPL")
 
     purchase_year = "1900"
     # sell_year = "2023"
@@ -16,16 +25,14 @@ def main():
     shares = 1000
     balance = 0
 
-    """
-        Get stock (use for dividends, current price, etc.)
-    """
+    ticker = stock_symbol
+    purchase_year = start_date[:4]
+    sell_year = end_date[:4]
+
     stock = yf.Ticker(ticker)
 
-    print(f'Test: {ticker}, {purchase_year} --> {sell_year}.')
+    # print(f'Test: {ticker}, {purchase_year} --> {sell_year}.')
 
-    """
-        Get dividends between a specified period, two string objects [yyyy:yyyy].
-    """
     dividends = stock.dividends[purchase_year:sell_year]
     df = pd.DataFrame(columns=['Date', 'Shares', 'Total Value'])
 
@@ -42,13 +49,11 @@ def main():
     #     "Current Balance"
     # ]
 
-    """
-        Get the stock price from the date given by the current quarter of dividend price. I.e. dividend price
-        is given by quarter with a specified date for that quarter.
-    """
     start_year = str(purchase_year) + "-01-01"
     end_year = str(sell_year) + "-12-31"
     stock_history = yf.download(ticker, start_year, end_year)["Close"]
+    a = stock_history[0] * shares
+    total_value = 0
 
     for quarter_date, dividend in dividends.items():
         quarter_date = str(quarter_date).split(' ')[0]
@@ -82,11 +87,18 @@ def main():
     # print(table)
     df.set_index('Date', inplace=True)
 
-    print(df)
+    # print(df)
     st.title('Welcome to Ryan.O.C Compound Interest Calculator!\n')
 
     st.line_chart(df['Total Value'])
 
+    s = "${0:,.2f}".format(a)
+    st.write("""start with """, s)
+    e = "${0:,.2f}".format(total_value)
+    st.write("""current value """, e)
+
+    st.write(""" # Total share growth over time""")
+    st.line_chart(df['Shares'])
 
 if __name__ == "__main__":
     main()
